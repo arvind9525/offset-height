fdc7b214333fe1df7e8f70f0c5975e1b02e7a24a
ARVIND KUMAR SHARMA <54414078+arvind9525@users.noreply.github.com>
2024-02-21 12:50:25 +0530

    Fix button in the bottom.
    for using after button go on the top and and show button in the button.

---
+++ offsset client project button fix.js
@@ -0,0 +1,18 @@
+// Need to add <script>  </script> if you are add this code in body
+jQuery(window).scroll(function() {
+ 
+var current_top = $(document).scrollTop();
+var scrollform = $('#row--16671').offset().top;
+var formHeight = $('#row--16671').height();
+var formBottom = scrollform + formHeight;
+
+var formSection = $('#section--40610').offset().top;
+
+if (current_top >= formBottom && current_top <= formSection) {
+    $('#section--16790').show();
+  	
+} else {
+    $('#section--16790').hide();
+	
+}
+});
